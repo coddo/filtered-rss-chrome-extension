dde6a73998683d013fe5e7b323be8b5508a6856e
Claudiu Codoban <codobanclaudiu@gmail.com>
2019-07-30 17:41:25 +0300

    Added a lambda function that will be used by the API gateway for proxying requests

---
+++ aws-lambda-proxy/handler.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+module.exports.proxyRequest = async (event) => {
+  const url = event.queryStringParameters.feed;
+  const response = await axios.get(url);
+  const agwResponse = {
+    statusCode: response.status,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+  };
+
+  if (response.status !== 200) {
+    agwResponse.body = `Error invoking feed: status ${response.status}, message ${response.data}`;
+    return Promise.reject(agwResponse);
+  }
+
+  agwResponse.body = response.data;
+  return Promise.resolve(agwResponse);
+}
