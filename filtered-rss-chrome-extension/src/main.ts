fafe9a52f734608344baa4556debc646ffa73b33
Claudiu Codoban <codobanclaudiu@gmail.com>
2019-09-01 20:14:05 +0300

    Improved the automated build scripts with configuration patching and added all the output JS files as background scripts

--- filtered-rss-chrome-extension/src/main.ts
+++ filtered-rss-chrome-extension/src/main.ts
@@ -1,10 +1,47 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { generateGuid } from "./ts/utils";
+import { Notifications } from "./ts/notifications";
 
 Vue.config.productionTip = false;
 
-new Vue({
-    router,
-    render: (h) => h(App),
-}).$mount("#app");
+try {
+    // declare lifecycle hooks listener functions
+    function onInstalledListener(): void {}
+
+    function onStartupListener(): void {}
+
+    function onSuspendListener(): void {
+        chrome.runtime.onInstalled.removeListener(onInstalledListener);
+        chrome.runtime.onStartup.removeListener(onStartupListener);
+        chrome.runtime.onSuspend.removeListener(onSuspendListener);
+    }
+
+    // register the lifecycle hooks
+    // chrome.runtime.onInstalled.addListener(onInstalledListener);
+    // chrome.runtime.onStartup.addListener(onStartupListener);
+    // chrome.runtime.onSuspend.addListener(onSuspendListener);
+
+    chrome.runtime.onInstalled.addListener(() => {
+        Notifications.createNotification(generateGuid(), "onInstalled", "", Date.now(), true);
+    });
+
+    chrome.runtime.onStartup.addListener(() => {
+        Notifications.createNotification(generateGuid(), "onStartup", "", Date.now(), true);
+    });
+
+    chrome.runtime.onSuspend.addListener(() => {
+        Notifications.createNotification(generateGuid(), "onSuspend", "", Date.now(), true);
+    });
+
+    chrome.runtime.onConnect.addListener(() => {
+        Notifications.createNotification(generateGuid(), "onConnect", "", Date.now(), true);
+    });
+} finally {
+    // create the Vue app
+    new Vue({
+        router,
+        render: h => h(App)
+    }).$mount("#app");
+}
---
+++ patch-config.ps1
@@ -0,0 +1,21 @@
+# Set the location at the root the extension
+Set-Location ..
+
+# Declare variables
+$BuildPath = "./filtered-rss-chrome-extension"
+
+# Remove previous build output
+Remove-Item -Recurse "css" -ErrorAction "SilentlyContinue" | Out-Null
+Remove-Item -Recurse "img" -ErrorAction "SilentlyContinue" | Out-Null
+Remove-Item -Recurse "js" -ErrorAction "SilentlyContinue" | Out-Null
+Remove-Item "favicon.png" -ErrorAction "SilentlyContinue" | Out-Null
+Remove-Item "index.html" -ErrorAction "SilentlyContinue" | Out-Null
+
+# Copy the new build output
+Copy-Item -Recurse -Path "$BuildPath/dist/*" -Destination "./"
+
+# Patch the extension manifest file
+$JsFiles = $(Get-ChildItem ".\js" | Where-Object { $_.extension -eq ".js" } | Select-Object -ExpandProperty Name | ForEach-Object {"`"./js/$($_)`""});
+$JsFilesListString = $($JsFiles -join ", ")
+
+$(Get-Content -Path ".\manifest_base.json") -replace "#{scripts_list}", "$JsFilesListString" | Set-Content -Path ".\manifest.json"
